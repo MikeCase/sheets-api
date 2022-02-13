a10dd038539e25132c6c6cf7d8d6fa8a72d0e517
MikeCase <michael.e.case@gmail.com>
2022-02-12 21:09:19 -0600

    .

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class MySpreedsheet:
         self.sheets.add_emails_to_sheet(data)
 
     def get_emails(self):
-        return self.email.get_emails(max_results=500)
+        return self.email.get_emails(max_results=50)
 
 if __name__ == '__main__':
     ss = MySpreedsheet()
